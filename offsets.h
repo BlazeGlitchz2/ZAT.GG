ebaecbed50ddd031239f88922267292e62685a86
BlazeGlitchz2 <112781846+BlazeGlitchz2@users.noreply.github.com>
2024-12-16 15:29:00 +0300

    Add files via upload

---
+++ offsets.h
@@ -0,0 +1,21 @@
+#pragma once
+class offsets {
+
+};
+
+constexpr auto OFFSET_GWORLD = 0x170d9d88;				//https://dumpspace.spuckwaffel.com/Games/?hash=6b77eceb&type=offsets
+constexpr auto UGameInstance = 0x1f0;					//https://dumpspace.spuckwaffel.com/Games/?hash=6b77eceb&type=classes&idx=UWorld&member=OwningGameInstance
+constexpr auto Localplayers = 0x38;						//https://dumpspace.spuckwaffel.com/Games/?hash=6b77eceb&type=classes&idx=UGameInstance&member=LocalPlayers
+constexpr auto PlayerController = 0x30;					//https://dumpspace.spuckwaffel.com/Games/?hash=6b77eceb&type=classes&idx=UPlayer&member=PlayerController
+constexpr auto Player = 0x350;							//https://dumpspace.spuckwaffel.com/Games/?hash=6b77eceb&type=classes&idx=APlayerController&member=AcknowledgedPawn
+constexpr auto Rootcomponent = 0x1b0;					//https://dumpspace.spuckwaffel.com/Games/?hash=6b77eceb&type=classes&idx=AActor&member=RootComponent
+constexpr auto GameState = 0x178;						//https://dumpspace.spuckwaffel.com/Games/?hash=6b77eceb&type=classes&idx=UWorld&member=GameState
+constexpr auto PlayerArray = 0x2c0;						//https://dumpspace.spuckwaffel.com/Games/?hash=6b77eceb&type=classes&idx=AGameStateBase&member=PlayerArray
+//COULDNT FIND PRIVATEPAWN
+//uintptr_t Player = read<uintptr_t>(PlayerState + 0x308); //https://fn.dumps.host/?class=APlayerState&member=PawnPrivate
+///
+constexpr auto Localplayers = 0x38;						//
+constexpr auto Localplayers = 0x38;						//
+constexpr auto Localplayers = 0x38;						//
+constexpr auto Localplayers = 0x38;						//
+constexpr auto Localplayers = 0x38;						//
